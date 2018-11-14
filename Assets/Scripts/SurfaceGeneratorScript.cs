f79f07afa310aa326615fe97bf49e8099d9d1fce
Jakub Brehuv <kubek.breha@gmail.com>
2018-11-14 08:28:13 +0100

    plane in enviroment sort of finished, not tested tho

---
+++ Assets/Scripts/SurfaceGeneratorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GoogleARCore;
+
+public class SurfaceGeneratorScript : MonoBehaviour {
+    
+    public GameObject surfacePrefab;
+
+    private List<DetectedPlane> newPlanes = new List<DetectedPlane>();
+
+    void Update(){
+        if (Session.Status != SessionStatus.Tracking){
+            return;
+        }
+
+        Session.GetTrackables<DetectedPlane>(newPlanes, TrackableQueryFilter.New);
+        foreach (var plane in newPlanes){
+            GameObject planeObject = Instantiate(surfacePrefab, Vector3.zero, Quaternion.identity, transform);
+            //planeObject.GetComponent<Surface>().Initialize(plane);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/SurfaceScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GoogleARCore;
+
+public class SurfaceScript: MonoBehaviour {
+    
+    public DetectedPlane trackedPlane;
+
+    public MeshRenderer meshRenderer;
+    public Mesh mesh;
+
+	void Start () {
+        meshRenderer = GetComponent<MeshRenderer>();
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+	}
+	
+	void Update () {
+        if (trackedPlane == null) return;
+
+        if(trackedPlane.SubsumedBy != null){
+            Destroy(gameObject);
+            return;
+        }
+
+        if(trackedPlane.TrackingState != TrackingState.Tracking){
+            meshRenderer.enabled = false;
+            return;
+        }
+
+        meshRenderer.enabled = true;
+
+        UpdateVerticles();
+    
+    }
+
+
+    void UpdateVerticles(){
+        var vertices = new List<Vector3>();
+        trackedPlane.GetBoundaryPolygon(vertices);
+
+        vertices.Add(trackedPlane.CenterPose.position);
+        var triangles = new List<int>();
+       
+        for (int i = 0; i < vertices.Count -1 ; i++){
+            triangles.Add(vertices.Count - 1);
+            triangles.Add(i);
+            triangles.Add(i == vertices.Count - 2 ? 0 : i + 1);
+        }
+
+        mesh.SetVertices(vertices);
+        mesh.SetTriangles(triangles.ToArray(), 0);
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+}
